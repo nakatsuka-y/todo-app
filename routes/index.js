058582a505c4fbf6e0983fdd54c3507e3b649a03
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2017-03-13 20:25:14 +0900

    implemented task deletion function

--- routes/index.js
+++ routes/index.js
@@ -61,25 +61,54 @@ router.post('/updatetask', function(req, res, next) {
   var task = req.body.task;
   var checked = req.body.checked;
 
-  console.log(req.body);
-
   // Set collection
   var collection = db.get('todocollection');
 
   // Submit to DB
   collection.findOneAndUpdate(
-   {"task": task},
-   {$set: {"checked": checked, "task": task}},
-   function(err, doc){
-    if (err) {
-	  // If failed, return error
-	  req.send("There was a problem adding the information to the database");
-	}
-	else {
-	  // Forward to success page
-	  res.redirect("todolist");
-	}
-  });
+    {"task": task},
+    {$set: {"checked": checked, "task": task}},
+    function(err, doc){
+      if (err) {
+        // If failed, return error
+        req.send("There was a problem adding the information to the database");
+      }
+      else {
+        // Forward to success page
+        res.redirect("todolist");
+      }
+    }
+  );
+});
+
+/* POST delete task */
+router.post('/deletetask', function(req, res, next) {
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get form values
+  var task = req.body.task;
+  var checked = req.body.checked;
+  var del = req.body.del;
+  console.log(req.body)
+
+  // Set collection
+  var collection = db.get('todocollection');
+
+  // Submit to DB
+  collection.remove(
+    {"task": task},
+    function(err, doc){
+      if (err) {
+        // If failed, return error
+        req.send("There was a problem adding the information to the database");
+      }
+      else {
+        // Forward to success page
+        res.redirect("todolist");
+      }
+    }
+  );
 });
 
 module.exports = router;
