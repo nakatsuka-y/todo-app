8b6e61dbcea45e5f659e85140b1e6e970b956c95
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2017-03-13 15:48:58 +0900

    created todo app framework

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/todo-app');
+
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +26,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make DB accessible to router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,84 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/* router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ToDo List App' });
+}); */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect("todolist")
+});
+
+/* GET todo list */
+router.get('/todolist', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('todocollection');
+  collection.find({},{},function(e,docs){
+    res.render('todolist', {
+	  "todolist": docs
+	});
+  });
+});
+
+/* GET new task */
+router.get('/newtask', function(req, res, next) {
+  res.render('newtask', { title: 'Add New Task' });
+});
+
+/* POST add task */
+router.post('/addtask', function(req, res, next) {
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get form values
+  var task = req.body.task;
+
+  // Set collection
+  var collection = db.get('todocollection');
+
+  // Submit to DB
+  collection.insert({
+    "checked": false,
+    "task": task
+  }, function(err, doc){
+    if (err) {
+	  // If failed, return error
+	  req.send("There was a problem adding the information to the database");
+	}
+	else {
+	  // Forward to success page
+	  res.redirect("todolist");
+	}
+  });
+});
+
+/* POST update task */
+router.post('/updatetask', function(req, res, next) {
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get form values
+  var task = req.body.task;
+  var checked = req.body.checked;
+
+  console.log(req.body);
+
+  // Set collection
+  var collection = db.get('todocollection');
+
+  // Submit to DB
+  collection.findOneAndUpdate(
+   {"task": task},
+   {$set: {"checked": checked, "task": task}},
+   function(err, doc){
+    if (err) {
+	  // If failed, return error
+	  req.send("There was a problem adding the information to the database");
+	}
+	else {
+	  // Forward to success page
+	  res.redirect("todolist");
+	}
+  });
 });
 
 module.exports = router;
